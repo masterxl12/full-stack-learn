4e7b200d225b7547c21cbbadfa176527bccaa758
xl <leixiao565566@163.com>
2022-06-21 00:04:42 +0800

    feat:vite配置

---
+++ vite/vite-train/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <HelloWorld message="123" />
+    <img src="./assets/avatar.jpg" />
+    <img :src="imgUrl" />
+    <div :class="style.wrapper">
+      <p>wrapper</p>
+      <div :class="style.inner">
+        <p>inner</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+import imgUrl from "./assets/avatar.jpg";
+import style from "./styles/style.module.less";
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vite/vite-train/src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script lang="ts">
+console.log(1234);
+</script>
